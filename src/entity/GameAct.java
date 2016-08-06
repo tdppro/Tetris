c352bb2c17364397daa22adc7818662d626e99a4
tdppro <tdppro@163.com>
2016-08-06 21:47:33 +0800

    20160806

--- src/entity/GameAct.java
+++ src/entity/GameAct.java
@@ -1,5 +1,27 @@
 package entity;
 
-public class GameAct {
+import java.awt.Point;
 
+public class GameAct {
+    private Point[] actPoint;
+    
+    public GameAct(){
+        //TODO ≈‰÷√Œƒº˛
+        actPoint = new Point[] {
+                new Point(3,0),
+                new Point(4,0),
+                new Point(5,0),
+                new Point(5,1),
+        };
+    }
+
+    public Point[] getActPoint() {
+        return actPoint;
+    }
+
+    public void setActPoint(Point[] actPoint) {
+        this.actPoint = actPoint;
+    }
+    
+    
 }
--- src/service/GameService.java
+++ src/service/GameService.java
@@ -1,11 +1,14 @@
 package service;
 
 import dto.GameDto;
+import entity.GameAct;
 
 public class GameService {
 	private GameDto dto;
 	public GameService(GameDto dto) {
 		this.dto = dto;
+		GameAct act = new GameAct();
+		dto.setGameAct(act);
 	}
     public void gameTest() {
         int temp = dto.getNowPoint();
